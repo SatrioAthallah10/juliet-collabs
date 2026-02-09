09ed5b0e392b4fbb9ffb213e6f0b26bdb664330f
SatrioAthallah10 <satrioathallah38@gmail.com>
2026-02-09 16:17:52 +0700

    Upload kode pertama

---
+++ App code/e-school-saas-student-parent/e-school-saas/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:eschool/app/app.dart';
+
+///[V.1.8.2]
+
+Future<void> main() async {
+  await initializeApp();
+}
---
+++ App code/eschool-saas-staff/eschool-saas-staff/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:eschool_saas_staff/app/app.dart';
+
+//[V.1.8.2]
+
+void main() async {
+  await initializeApp();
+}
